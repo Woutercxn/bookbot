5fb3e81ab6721a4056c8820d1dd440e19e7e62a4
Wouter Benoit <wbenoit@conxion.be>
2025-10-15 16:43:40 +0200

    Read and print content from Frankenstein book file

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+path_to_file = "books/frankenstein.txt"
+
+with open(path_to_file, "r") as f:
+    content = f.read()
+    print(content)
+# This code reads the content of the file "frankenstein.txt" located in the "books" directory 
+# and prints it to the console.
