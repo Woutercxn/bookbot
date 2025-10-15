80ffda08706a2fb3793ce88fc0188d4ba9219d00
Wouter Benoit <wbenoit@conxion.be>
2025-10-15 16:46:46 +0200

    Refactor file reading logic into a function

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-path_to_file = "books/frankenstein.txt"
-
-with open(path_to_file, "r") as f:
-    content = f.read()
-    print(content)
+def get_book_text(path_to_file: str):
+    with open(path_to_file, "r") as f:
+        content = f.read()
+        print(content)
 # This code reads the content of the file "frankenstein.txt" located in the "books" directory 
 # and prints it to the console.
+
+def main():
+    path_to_file = "books/frankenstein.txt"
+    get_book_text(path_to_file)
+
+main()
