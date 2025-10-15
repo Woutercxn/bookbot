da7f336ce21a425376cc6c35b5466c7676e13998
Wouter Benoit <wbenoit@conxion.be>
2025-10-15 17:12:06 +0200

    Refactor get_book_text function to return content instead of printing, and update main to use returned text for word count calculation

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def get_book_text(path_to_file: str):
+def get_book_text(path_to_file: str) -> str:
     with open(path_to_file, "r") as f:
         content = f.read()
-        print(content)
-# This code reads the content of the file "frankenstein.txt" located in the "books" directory 
-# and prints it to the console.
+        return content
 
 def count_words(text: str) -> int:
     """
@@ -21,8 +19,8 @@ def count_words(text: str) -> int:
 def main():
     path_to_file = "books/frankenstein.txt"
 
-    get_book_text(path_to_file)
-    wordcount = count_words(path_to_file)
+    text = get_book_text(path_to_file)
+    wordcount = count_words(text)
     print(f"Word count: {wordcount}")
 
 main()
